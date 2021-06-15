a5fa0fb8f96cf0c824caa3ef9f2a8177769331a1
rromang <80008461+rromang@users.noreply.github.com>
2021-06-14 18:30:06 -0700

    update scrape python file

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,26 +1,56 @@
 from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-import scrape_mars
+from flask_pymongo import pymongo
+# import scrape_mars
 
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-conn = 'mongodb://localhost:27017'
+#example from: https://www.geeksforgeeks.org/mongodb-python-insert-update-data/
+try:
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    print('Connected successfully!')
+except:
+    print('Could not connect to MongoDB')
 
-client = PyMongo.MongoClient(conn)
 
-db = client.mars_db
+# database
+mars_db = myclient.database
+  
+# Created or Switched to collection names: my_gfg_collection
+collection = mars_db.my_gfg_collection
+  
+emp_rec1 = {
+        "name":"Mr.Geek",
+        "eid":24,
+        "location":"delhi"
+        }
+emp_rec2 = {
+        "name":"Mr.Shaurya",
+        "eid":14,
+        "location":"delhi"
+        }
+  
+# Insert Data
+rec_id1 = collection.insert_one(emp_rec1)
+rec_id2 = collection.insert_one(emp_rec2)
+  
+print("Data inserted with record ids",rec_id1," ",rec_id2)
+  
+# Printing the data inserted
+cursor = collection.find()
+for record in cursor:
+    print(record)
 
 # Route to render index.html template using data from Mongo
-@app.route("/")
-def home():
+# @app.route("/")
+# def home():
 
 
 
-# Route that will trigger the scrape function
-@app.route("/scrape")
+# # Route that will trigger the scrape function
+# @app.route("/scrape")
  
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -1,62 +1,29 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
+# define scrape function
 def scrape():
     from bs4 import BeautifulSoup as bs
-
-
-    # In[2]:
-
-
+    #create variable with website to visit
     url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
-
-
-    # In[95]:
-
 
     from selenium import webdriver
     # followed example from https://medium.com/ymedialabs-innovation/web-scraping-using-beautiful-soup-and-selenium-for-dynamic-page-2f8ad15efe25
-    options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--incognito')
-    options.add_argument('--headless')
-    driver = webdriver.Chrome("/Users/rosaicelaroman/Desktop/Data_BootCamp/APIs/chromedriver", options=options)
-
-
-    # In[4]:
-
-
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('--ignore-certificate-errors')
+    # options.add_argument('--incognito')
+    # options.add_argument('--headless')
+    driver = webdriver.Chrome("/Users/rosaicelaroman/Desktop/Data_BootCamp/APIs/chromedriver")
     driver.get(url)
-
-
-    # In[5]:
-
-
     page_source = driver.page_source
-
-
-    # In[6]:
-
-
+    #after opening web browser, get page_source
     soup = bs(page_source, 'lxml')
 
-
-    # In[7]:
-
-
-    # print(soup.prettify())
-
-
-    # In[8]:
-
-
+    # open empty list to include all news articles found in the page
     news_articles = []
     par_articles = []
-
+    #loop to find all articled and article bodies from page_source and append them to lists
     articles_finder = soup.find_all('div', class_='list_text')
-
     for article in articles_finder:
         division = article.find('div', class_='content_title')
         title = division.find('a').get_text()
@@ -68,37 +35,22 @@ def scrape():
     print(par_articles)
 
 
-    # In[9]:
-
-
+    #obtain the first news title and article from the list
     news_title = news_articles[0]
-
     news_par = par_articles[0]
 
     print(news_title)
     print(news_par)
 
-    # featured_img_url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/image/featured/mars1.jpg'
-
-
-    # In[12]:
-
-
-    #example found in https://evancalz.medium.com/web-automation-with-splinter-db2fe006ea45
 
     from splinter import Browser
     import time
 
     URL = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
-    # un-comment this if you are using Windows!
-    # executable_path = {'executable_path': 'chromedriver.exe'}
-    # browser = Browser('chrome', **executable_path)
-    # this will open up a new headless browser and navigate to our URL
-    browser = Browser('chrome')
+   
     browser.visit(URL)
-    # tell python to pause for 10s so we can observe the browser
-
-    # first_found = browser.find_by_partial_text('featured').first
+    
+   
     links_found = browser.click_link_by_partial_href('featured')
     image_url = browser.url
     # print(image_url)
@@ -106,14 +58,11 @@ def scrape():
     browser.quit()
 
 
-    # In[13]:
 
 
     featured_image_url = image_url
     print(featured_image_url)
 
-
-    # In[14]:
 
 
     import pandas as pd
@@ -124,51 +73,21 @@ def scrape():
     mars_facts = pd.read_html(pd_url)
 
 
-    # In[15]:
-
-
-    # print(len(mars_facts))
-
-
-    # In[16]:
-
-
-    # print(mars_facts[0])
-
-
-    # In[22]:
-
-
     df = mars_facts[0]
     mars_facts_df = df.rename(columns={0: "Parameters", 1: "Values"})
     mars_facts_df
 
 
-    # In[32]:
 
     image_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     driver.get(image_url)
 
 
-    # In[33]:
-
-
     page_source = driver.page_source
-
-
-    # In[34]:
 
 
     soup = bs(page_source, 'lxml')
 
-
-    # In[35]:
-
-
-    # print(soup.prettify())
-
-
-    # In[54]:
 
 
     image_links = []
@@ -180,11 +99,6 @@ def scrape():
         link = link.a['href']
     
         image_links.append(link)
-        
-    # print(image_links)
-
-
-    # In[123]:
 
 
 
@@ -202,11 +116,6 @@ def scrape():
         
 
     browser.quit()
-    # print(img_urls)
-
-
-    # In[162]:
-
 
 
     images = []
@@ -224,12 +133,10 @@ def scrape():
     
 
 
-    # print(images)
 
     time.sleep(10)
 
 
-    # In[167]:
 
 
     titles = []
@@ -247,12 +154,12 @@ def scrape():
             titles.append(link)
 
 
-    # print(titles)
+
 
     time.sleep(10)
 
 
-    # In[176]:
+
 
 
     img_info = []
@@ -267,8 +174,5 @@ def scrape():
 
 
 scrape()
-# In[ ]:
-
-
 
 
