71a156f7dad7c271e478724db676f61dfcaea42a
rromang <80008461+rromang@users.noreply.github.com>
2021-06-16 13:22:37 -0700

    created index html changed app.py

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -4,6 +4,7 @@ from scrape_mars import *
 
 # Create an instance of Flask
 app = Flask(__name__)
+  
 
 # Use PyMongo to establish Mongo connection
 #example from: https://www.geeksforgeeks.org/mongodb-python-insert-update-data/
@@ -20,65 +21,43 @@ print("Database created........")
 print("List of databases after creating new one")
 print(client.list_database_names())
 
+
 MarsInfo = db.MarsInfo
 
-# print(db.list_collection_names())
+# # print(db.list_collection_names())
 
-data = scrape()
+# data = scrape()
 
-MarsInfo.insert_many(data)
-# print(data)
+# MarsInfo.insert_many(data)
 
-# db.MarsInfo.insert_one(data.maps)
-
-cursor = db.MarsInfo.find()
-for record in cursor:
-    print(record)
-
-# print(type(data))
-
-
-# data = [{'title_news': ["NASA's Ingenuity Mission Honored by the Space Foundation"], 'article_par': ['The mission picked up the 2021 John L. “Jack” Swigert, Jr., Award for Space Exploration for its history-making achievements.'], 'featured_image': 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html', 'mars_facts':              Parameters                         Values
-# 0  Equatorial Diameter:                       6,792 km
-# 1       Polar Diameter:                       6,752 km
-# 2                 Mass:  6.39 × 10^23 kg (0.11 Earths)
-# 3                Moons:            2 (Phobos & Deimos)
-# 4       Orbit Distance:       227,943,824 km (1.38 AU)
-# 5         Orbit Period:           687 days (1.9 years)
-# 6  Surface Temperature:                   -87 to -5 °C
-# 7         First Record:              2nd millennium BC
-# 8          Recorded By:           Egyptian astronomers, 'mars_images_full': [{'title': 'Cerberus Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg'}, {'title': 'Cerberus Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif/full.jpg'}, {'title': 'Cerberus Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif/full.jpg'}, {'title': 'Cerberus Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif/full.jpg'}, {'title': 'Schiaparelli Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg'}, {'title': 'Schiaparelli Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif/full.jpg'}, {'title': 'Schiaparelli Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif/full.jpg'}, {'title': 'Schiaparelli Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif/full.jpg'}, {'title': 'Syrtis Major Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg'}, {'title': 'Syrtis Major Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif/full.jpg'}, {'title': 'Syrtis Major Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif/full.jpg'}, {'title': 'Syrtis Major Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif/full.jpg'}, {'title': 'Valles Marineris Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg'}, {'title': 'Valles Marineris Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif/full.jpg'}, {'title': 'Valles Marineris Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/syrtis_major_enhanced.tif/full.jpg'}, {'title': 'Valles Marineris Hemisphere Enhanced', 'img_url': 'https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/valles_marineris_enhanced.tif/full.jpg'}]}]
-
-# data = [{'title_news': ["NASA's Ingenuity Mission Honored by the Space Foundation"], 'article_par': ['The mission picked up the 2021 John L. “Jack” Swigert, Jr., Award for Space Exploration for its history-making achievements.'], 'featured_image': 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'}]
-# data_newstitle = {data[0]}
-
-# data_dict = {}
-# for x in data:
-#     data_dict.update(x)
-# print(data_dict)
-
-
-# MarsInfo.insert_one(data_dict)
-
-
-#print list of the _id values of the inserted documents:
-
-# db.MarsInfo.insert_one(data_dict)
-
-# cursor = db.MarsInfo.find()
-# for record in cursor:
-#     print(record)
   
 # Printing the data inserted
 # Route to render index.html template using data from Mongo
-# @app.route("/")
-# def home():
+@app.route("/")
+def home():
 
+    # Find one record of data from the mongo database
+    mars_info = db.MarsInfo.find()
+    
+
+    
+    # Return template and data
+    return render_template("index.html", mars_data=mars_info)
 
 
 # # Route that will trigger the scrape function
-# @app.route("/scrape")
- 
+@app.route("/scrape")
+def scrape():
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+    mars_data = scrape.scrape_info()
+
+    # MarsInfo.insert_many(data)
+    MarsInfo.update({}, mars_data, upsert=True)
+
+
+    # Redirect back to home page
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
