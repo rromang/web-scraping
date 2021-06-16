d41ddd500d33d270ecb729c9a56d1ffc93dbf394
rromang <80008461+rromang@users.noreply.github.com>
2021-06-15 23:06:31 -0700

    update for app and scraping file

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import pymongo
-# import scrape_mars
+from scrape_mars import *
 
 # Create an instance of Flask
 app = Flask(__name__)
@@ -8,40 +8,38 @@ app = Flask(__name__)
 # Use PyMongo to establish Mongo connection
 #example from: https://www.geeksforgeeks.org/mongodb-python-insert-update-data/
 try:
-    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    conn = "mongodb://localhost:27017/"
+    client = pymongo.MongoClient(conn)
     print('Connected successfully!')
 except:
     print('Could not connect to MongoDB')
 
+db = client['marsDB']
+print("Database created........")
 
-# database
-mars_db = myclient.database
-  
-# Created or Switched to collection names: my_gfg_collection
-collection = mars_db.my_gfg_collection
-  
-emp_rec1 = {
-        "name":"Mr.Geek",
-        "eid":24,
-        "location":"delhi"
-        }
-emp_rec2 = {
-        "name":"Mr.Shaurya",
-        "eid":14,
-        "location":"delhi"
-        }
-  
-# Insert Data
-rec_id1 = collection.insert_one(emp_rec1)
-rec_id2 = collection.insert_one(emp_rec2)
-  
-print("Data inserted with record ids",rec_id1," ",rec_id2)
+print("List of databases after creating new one")
+print(client.list_database_names())
+
+MarsInfo = db.MarsInfo
+
+# print(db.list_collection_names())
+
+data = [scrape()]
+
+print(data)
+print(type(data))
+
+x = db.MarsInfo.insert_many([item for item in data])
+
+#print list of the _id values of the inserted documents:
+print(x.inserted_ids)
+# db.MarsInfo.insert_one(data)
+
+# cursor = db.MarsInfo.find()
+# for record in cursor:
+#     print(record)
   
 # Printing the data inserted
-cursor = collection.find()
-for record in cursor:
-    print(record)
-
 # Route to render index.html template using data from Mongo
 # @app.route("/")
 # def home():
--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -1,31 +1,31 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# define scrape function
+#Start beautiful soup
 def scrape():
     from bs4 import BeautifulSoup as bs
-    from splinter import browser
-    import time
-    #create variable with website to visit
+
+    #Define variable for url for news page for NASA
     url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
 
+    #start webdriver to connect to chrome and open the url
     from selenium import webdriver
     # followed example from https://medium.com/ymedialabs-innovation/web-scraping-using-beautiful-soup-and-selenium-for-dynamic-page-2f8ad15efe25
-    # options = webdriver.ChromeOptions()
-    # options.add_argument('--ignore-certificate-errors')
-    # options.add_argument('--incognito')
-    # options.add_argument('--headless')
-    driver = webdriver.Chrome("/Users/rosaicelaroman/Desktop/Data_BootCamp/APIs/chromedriver")
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument('--incognito')
+    options.add_argument('--headless')
+    driver = webdriver.Chrome("/Users/rosaicelaroman/Desktop/Data_BootCamp/APIs/chromedriver", options=options)
+
     driver.get(url)
     page_source = driver.page_source
-    #after opening web browser, get page_source
     soup = bs(page_source, 'lxml')
 
-    # open empty list to include all news articles found in the page
+    #Initialize list for article titles and paragraphs
     news_articles = []
     par_articles = []
-    #loop to find all articled and article bodies from page_source and append them to lists
+
+    #Use soup to parse through page; create loop to add the articles and paragraphs to the lists 
     articles_finder = soup.find_all('div', class_='list_text')
+
     for article in articles_finder:
         division = article.find('div', class_='content_title')
         title = division.find('a').get_text()
@@ -33,61 +33,62 @@ def scrape():
         par_division = article.find('div', class_='article_teaser_body')
         par = par_division.get_text()
         par_articles.append(par)
-    print(news_articles)
-    print(par_articles)
+    print('--------- News Article Titles Found!')
+    print('--------- News Article Titles Paragraphs Found!')
+
+    #create variables for the first article and paragraph
+    news_title = [news_articles[0]]
+    news_par = [par_articles[0]]
+
+    print('--------- First Article Title Found!')
+    print('--------- First Article Paragraph Found!')
 
 
-    #obtain the first news title and article from the list
-    news_title = news_articles[0]
-    news_par = par_articles[0]
+    #---------------------------------
+    #Find featured image:
+    #example found in https://evancalz.medium.com/web-automation-with-splinter-db2fe006ea45
 
-    print(news_title)
-    print(news_par)
+    #import splinter to interact with browser
+    from splinter import Browser
+    import time
 
-    # store website
-    URL = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
-   
-    browser.visit(URL)
-    
-   
+    next_URL = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    # un-comment this if you are using Windows!
+    # executable_path = {'executable_path': 'chromedriver.exe'}
+    # browser = Browser('chrome', **executable_path)
+    # this will open up a new headless browser and navigate to our URL
+    browser = Browser('chrome')
+    browser.visit(next_URL)
+    # tell python to pause for 10s so we can observe the browser
+
+    # first_found = browser.find_by_partial_text('featured').first
     links_found = browser.click_link_by_partial_href('featured')
     image_url = browser.url
-    # print(image_url)
-    time.sleep(10)
+    featured_url = image_url
+    print('--------- Featured Image Found!')
+    time.sleep(5)
     browser.quit()
 
-
-
-
-    featured_image_url = image_url
-    print(featured_image_url)
-
-
-
+    #----------------------------------
+    #Create dataframe from webpage content
     import pandas as pd
     #example followed: https://pythonbasics.org/pandas-web-scraping/
 
     pd_url = 'https://space-facts.com/mars/'
 
     mars_facts = pd.read_html(pd_url)
-
-
     df = mars_facts[0]
     mars_facts_df = df.rename(columns={0: "Parameters", 1: "Values"})
     mars_facts_df
 
+    print('--------- Mars Facts Found!')
 
-
+    #----------------------------------
+    #Interact with the hemisphere webpage to get the url for the full image of the hemispheres
     image_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     driver.get(image_url)
-
-
     page_source = driver.page_source
-
-
     soup = bs(page_source, 'lxml')
-
-
 
     image_links = []
 
@@ -98,8 +99,8 @@ def scrape():
         link = link.a['href']
     
         image_links.append(link)
-
-
+        
+    print('--------- Image Links Found!')
 
     base_url = 'https://astrogeology.usgs.gov'
 
@@ -111,11 +112,8 @@ def scrape():
         browser.visit(base_url + link)
         img_urls.append(browser.url)
         
-        time.sleep(30)
-        
-
+        time.sleep(15)
     browser.quit()
-
 
     images = []
     for url in img_urls:
@@ -128,15 +126,9 @@ def scrape():
     
             link = link.a['href']
             images.append(link)
-    
-    
-
-
 
     time.sleep(10)
-
-
-
+    print('--------- List of Image Links Created!')
 
     titles = []
 
@@ -152,14 +144,9 @@ def scrape():
             link = link.find('h2', class_='title').text
             titles.append(link)
 
-
-
-
     time.sleep(10)
 
-
-
-
+    print('--------- List of Image Titles Created!')
 
     img_info = []
     for title in titles:
@@ -169,9 +156,13 @@ def scrape():
             dict_info['title'] = title
             dict_info['img_url'] = image
             img_info.append(dict_info)
-    print(img_info)
 
+    print('--------- Dictionary of Images and Titles Created!')
+    print('--------------Process Done!')
+    
+    dict_mars = {'title_news': news_title, 'article_par': news_par, 'featured_image': featured_url, 'mars_facts': mars_facts_df,
+    'mars_images_full': img_info}
+    return dict_mars
+    # print(dict_mars)
 
-scrape()
-
-
+# scrape()
